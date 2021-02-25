de600bb8cebd47e99c4a032eb887d8d40102ecf5
nikgun61 <73181274+nikgun61@users.noreply.github.com>
2021-02-25 15:34:54 +0300

    Update Program.cs

--- OS_lab_1/Program.cs
+++ OS_lab_1/Program.cs
@@ -1,5 +1,6 @@
-﻿// https://hackmd.io/@0x41/OS_Lab_1
+//https://hackmd.io/@0x41/OS_Lab_1
 using System;
+using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -10,46 +11,51 @@ namespace OS_lab_1
         unsafe static void Main(string[] args)
         {
             StringComparer comparer = StringComparer.OrdinalIgnoreCase; //для сравнения строк без учёта регистра
-            string Input;
-            string UserCommand;
-            string UserVar;
-
+            
             int MaxCommands = 3;
-            string[,] command = new string[MaxCommands, 3]; //Массив для команд
-            // 0 - имя команды, 1 - вызов функции, 2 - описание для help
+            int ComID = 0;
+            string UserPath = null;
+
+            string[,] command = new string[MaxCommands, 2]; //Названия и описание
+            
             command[0, 0] = "exit";
 
-            void (*message) ();
-            message = LogicalDrivesInfo;
-
-
             command[1, 0] = "LogicalDrivesInfo";
-           // command[1, 1] = ;
-
+            
             command[2, 0] = "fuck";
             MaxCommands--; //убираю одно значение для цикла for (костыль (^__^'))
 
 
             //Алгоритм для введения команд (до exit)
-            Input = Console.ReadLine();
-            UserCommand = GetUserCommand(Input);
-            UserVar = GetUserVar(Input, UserCommand);
+            string Input = Console.ReadLine();
+            string UserCommand = GetUserCommand(Input);
+            string UserVar = GetUserVar(Input, UserCommand);
             while (string.Compare(UserCommand, command[0, 0], true) != 0)
             {
                 for (int i = 1; i <= MaxCommands; i++)
                 {
                     if (string.Compare(UserCommand, command[i, 0], true) == 0)
                     {
-                        Console.WriteLine(UserVar);
-                        //вызов функции command[i, 1]
+                        ComID = i;
                         break;
-                    }
-                    else if (i == MaxCommands)
-                    {
-                        Console.WriteLine("Неизвестная команда. Для обзора доступных команд введите help");
-                    }
+                    }    
                 }
+                switch (ComID)
+                {
+                    case 0:
+                        Console.WriteLine("Неизвестная команда'" + UserCommand + "'. Для обзора доступных команд введите help");
+                        break;
 
+                    case 1:
+                        UserPath = LogicalDrivesInfo();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("hey you, mazafuka");
+                        break;
+                }
+                ComID = 0;
+                
                 Input = Console.ReadLine();
                 UserCommand = GetUserCommand(Input);
                 UserVar = GetUserVar(Input, UserCommand);
@@ -58,6 +64,7 @@ namespace OS_lab_1
             Environment.Exit(0);
         }
 
+        
         public static string GetUserCommand(string Input)
         {
             int length = Input.Length;
@@ -77,12 +84,17 @@ namespace OS_lab_1
         public static string GetUserVar(string Input, string UserCommand)
         {
             int length = UserCommand.Length;
-            string Output = Input.Remove(0, ++length);
-            return Output;
+            if (Input.Length == length)
+                return null;
+            else
+            {
+                string Output = Input.Remove(0, ++length);
+                return Output;
+            }
         }
 
         //1. Вывести информацию в консоль о логических дисках, именах, метке тома, размере типе файловой системы.
-        void LogicalDrivesInfo()
+        static string LogicalDrivesInfo()
         {
             DriveInfo[] drives = DriveInfo.GetDrives();
 
@@ -97,6 +109,32 @@ namespace OS_lab_1
                     Console.WriteLine($"Метка: {drive.VolumeLabel}");
                 }
             }
+
+            return null;
+        }
+        static string NewCatalog(string UserPath, string Uservar)
+        {
+            string path = @"C:\SomeDir";
+            int length = UserPath.Length;
+            int WrightPath1 = 0;
+            var chars = UserPath.ToCharArray();
+            for (int i = 0; i <= length; i++)
+            {
+                if (chars[i] == ':')
+                    WrightPath1++;
+                else if (chars[i] == '\' && WrightPath1 == 1)
+
+
+            }
+            string subpath = @"program\avalon";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+
+            return null;
         }
     }
 }
